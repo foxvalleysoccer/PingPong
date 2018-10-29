7e31af1d92504948d47087ef98dc57d6a9cb6efc
Janikowski, Josh <janikows@fvtc.edu>
2018-10-29 06:47:44 -0500

    time to text multiplayer

---
+++ Assets/SetPlayerAndCameraLocation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPlayerAndCameraLocation : MonoBehaviour {
+
+    GameObject mixedrealitycameraParentPlayer;
+    public GameObject Spawn1;
+
+	// Use this for initialization
+	void Start () {
+        mixedrealitycameraParentPlayer = GameObject.FindWithTag("MainCamera");
+        mixedrealitycameraParentPlayer.transform.position = Spawn1.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
